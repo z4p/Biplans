106cf19cc722e77d1b7c7e9309ff411b717f6c85
z4p <z4p@tut.by>
2014-03-23 21:27:30 +0300

    Biplans 0.1 alfa
    Стрельба с обработкой коллизий.
    Система здоровья для бипланов (с отображением)
    В движок добавлены статические анимации.
    Анимация взрыва при "смерти" биплана.

--- biplans.js
+++ biplans.js
@@ -1,20 +1,48 @@
 engine = function() {}
 
-var G = 0.5;
+var G = 0.4;
 
+// fast Math.sign
 Math.sign = function(x) {
     return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN : NaN;
 }
 
-function bullet(ctx, img, x, y, angle) {
-	this.maxV = 200;
-	this.img = img;
+// Animation object
+function Animation(ctx, img, frameSize, frLen, startX, startY) {
+	this.timeline = 0;
 	this.ctx = ctx;
-	this.vx = this.maxV * Math.cos(angle*Math.PI/6);
-	this.vy = -this.maxV * Math.sin(angle*Math.PI/6);
-	this.X = Math.floor(x);
-	this.Y = Math.floor(y);
-	this.enable = true;
+	this.img = img;
+	this.frame = 0;
+	this.frameSize = frameSize;
+	this.X = startX;
+	this.Y = startY;
+	this.fLength = frLen; // length of frame (sec)
+	this.framesCount = Math.floor(this.img.width / this.frameSize);
+	this.finished = false;
+	
+	this.draw = function() {
+		this.ctx.drawImage(this.img, this.frame*this.frameSize, 0, this.frameSize, this.frameSize, this.X, this.Y, this.frameSize, this.frameSize);
+	}
+	
+	this.tick = function(dt) {
+		this.timeline += dt;
+		this.frame = Math.floor(this.timeline / this.fLength);
+		if (this.frame > this.framesCount) {
+			this.finished = true;
+		}
+	}
+}
+
+// Bullet object
+function Bullet(ctx, img, x, y, angle) {
+	this.maxV = 200;	// bullet's speed
+	this.img = img;		// bullet's image
+	this.ctx = ctx;		// drawing context
+	this.vx = this.maxV * Math.cos(angle*Math.PI/6);	// speed X / sec
+	this.vy = -this.maxV * Math.sin(angle*Math.PI/6);	// speed Y / sec
+	this.X = Math.floor(x);	// pos X
+	this.Y = Math.floor(y);	// pos Y
+	this.enable = true;		// marker for erasing
 	console.log("New bullet "+this.X+':'+this.Y+' d('+this.vx+':'+this.vy+') a('+angle+')');
 	
 	this.draw = function() {
@@ -29,17 +57,18 @@ function bullet(ctx, img, x, y, angle) {
 			this.enable = false; //so, now we should forgive this bullet ;-(
 		}
 	}
+	
+	this.tick(0.25);
 }
 
-function biplane(ctx, img_src, bullet_src, orient, bullets) {
+// Biplane object
+function Biplane(ctx, img_bp, img_bullet, orient, bullets) {
 	this.orient = orient;			// Orientation is [direct | reverse] = [true | false]
 	this.width = 50; 				// width of plane, we're drawing
 	this.heigth = this.width/2;		// heigth of plane, we're drawing
-	this.img = new Image(); 		// biplan's image
-	this.img.src = img_src;
+	this.img = img_bp; 				// biplan's image
 	this.bullets = bullets;			// array of bullets
-	this.bullet_img = new Image(); 	// bullet's image
-	this.bullet_img.src = bullet_src;
+	this.bullet_img = img_bullet; 	// bullet's image
 	this.ctx = ctx;		   			// drawing context
 	this.X = orient ? 20 : 620-this.width; 	// position X (left border)
 	this.Y = 450 - this.heigth;		// position Y (top border)
@@ -51,6 +80,7 @@ function biplane(ctx, img_src, bullet_src, orient, bullets) {
 	this.vy = 0; 					// speed Y / sec
 	this.V = 0;						// speed
 	this.dir = 0;					// speed's direction
+	this.health = 3;				// level of health
 	this.reloadTime = 1;			// time to reload
 	this.reload = 1;				// reload progress [0..reloadTime]
 	this.isSky = false; 			// is this plane in the sky?
@@ -59,12 +89,16 @@ function biplane(ctx, img_src, bullet_src, orient, bullets) {
 	
 	this.incPower = function() {
 		this.F += 1;
-		if (this.F > 3) this.F = 3;
+		if (this.F > this.health) {
+			this.F = this.health;
+		}
 	}
 	
 	this.decPower = function() {
 		this.F -= 1;
-		if (this.F < 0) this.F = 0;
+		if (this.F < 0) {
+			this.F = 0;
+		}
 	}
 	
 	this.rotate = function(dr) { // dr = {-1,1}
@@ -100,11 +134,14 @@ function biplane(ctx, img_src, bullet_src, orient, bullets) {
 	this.shot = function() {
 		if (this.reload >= this.reloadTime) {
 			this.reload = 0;
-			this.bullets.push(new bullet(this.ctx, this.bullet_img, this.X+this.width/2, this.Y+this.heigth/2, this.angle));
+			this.bullets.push(new Bullet(this.ctx, this.bullet_img, this.X+this.width/2, this.Y+this.heigth/2, this.angle));
 		}
 	}
 	
-	this.step = function(dt) { // dt (sec) < 1 
+	this.step = function(dt) { // dt (sec) < 1
+		if (this.F > this.health) {
+			this.F = this.health;
+		}
 		var Fx = this.F * Math.cos(this.angle*Math.PI/6) ;
 		var Fy = -this.F * Math.sin(this.angle*Math.PI/6) + this.weight * G;
 		
@@ -140,6 +177,11 @@ function biplane(ctx, img_src, bullet_src, orient, bullets) {
 
 		this.X += this.vx*dt;
 		this.Y += this.vy*dt;
+		
+		if (this.health < 0) {
+			this.isAlive = false;
+			console.log("BOOOM!");
+		}
 		
 		if (this.Y < 400) {
 			this.isSky = true;
@@ -177,12 +219,16 @@ engine.prototype = {
     domElement: null,
     ctx: null,
     timer: null,
+	respTime: 8,
+	user1Resp: 0,
+	user2Resp: 0,
+	scores: {user1: 0, user2: 0},
 	user1: null,
 	user2: null,
+    images: [],
     biplanes: [],
 	bullets: [],		// array of bullets
-	bgimage: null,
-	bgimage_src: "images/bg.png",
+	animations: [],
     paused: false,
     audio: {
 		fly_down: null,
@@ -203,35 +249,45 @@ engine.prototype = {
         switch (key)
         {
             case 87: // W
-				this.user1.incPower();
+				if (this.user1)
+					this.user1.incPower();
                 break;
             case 83: // S
-				this.user1.decPower();
+				if (this.user1)
+					this.user1.decPower();
                 break;
             case 65: // A
-				this.user1.rotate(1);
+				if (this.user1)
+					this.user1.rotate(1);
                 break;
             case 68: // D
-				this.user1.rotate(-1);
+				if (this.user1)
+					this.user1.rotate(-1);
                 break;
             case 90: // Z
-                this.user1.shot();
+    			if (this.user1)
+					this.user1.shot();
                 break;
 
 			case 38: // up
-				this.user2.incPower();
+				if (this.user2)
+					this.user2.incPower();
                 break;
             case 40: // down
-				this.user2.decPower();
+				if (this.user2)
+					this.user2.decPower();
                 break;
             case 37: // left
-				this.user2.rotate(1);
+				if (this.user2)
+					this.user2.rotate(1);
                 break;
             case 39: // right
-				this.user2.rotate(-1);
+				if (this.user2)
+					this.user2.rotate(-1);
                 break;
-            case 190: // ?/
-                this.user2.shot();
+            case 190: // >.
+                if (this.user2)
+					this.user2.shot();
                 break;
         }
     },
@@ -248,11 +304,23 @@ engine.prototype = {
         });
         $(this.domElement).css('cursor','pointer');
         this.ctx = dom_element.getContext('2d');
-		this.ctx.font = "normal 14px sans-serif";
+		this.ctx.font = "bold 24px sans-serif";
+		this.ctx.fillStyle = "#ddd";
+		this.ctx.textAlign = "center";
 		
-		this.bgimage = new Image();
-		this.bgimage.src = this.bgimage_src;
-
+		this.images['bg'] = new Image();
+		this.images['bg'].src = 'images/bg.png';
+		this.images['bp1'] = new Image();
+		this.images['bp1'].src = 'images/biplan1.png';
+		this.images['bp2'] = new Image();
+		this.images['bp2'].src = 'images/biplan2.png';
+		this.images['bullet'] = new Image();
+		this.images['bullet'].src = 'images/bullet.png';
+		this.images['explosion'] = new Image();
+		this.images['explosion'].src = 'images/explosion.png';
+		this.images['heart'] = new Image();
+		this.images['heart'].src = 'images/heart.png';
+		
 		this.audio.fly_down = new Audio('sounds/fly_down.ogg');
 		this.audio.fly_up = new Audio('sounds/fly_up.ogg');
 		this.audio.fly_high1 = new Audio('sounds/fly_high.ogg');
@@ -271,16 +339,35 @@ engine.prototype = {
     redraw: function()
     {
 		var tm = (new Date()).getTime();
-		var dt = this.curTime - tm;
+		var dt = (this.curTime - tm) / 1000;
 		this.curTime = tm;
 		dt = 0.1;
 	
-        this.ctx.drawImage(this.bgimage, 0, 0);
+		// background image
+        this.ctx.drawImage(this.images['bg'], 0, 0);
+		
+		// game score
+		this.ctx.fillText(this.scores.user1+" - "+this.scores.user2, 320, 40);
 		
 		for (var i = 0; i < this.bullets.length; i++) {
+			if (!this.bullets[i])
+				continue;
 			this.bullets[i].tick(dt);
+			if (this.user1 &&
+				Math.abs(this.bullets[i].X - this.user1.X - this.user1.width/2) < this.user1.width/2 &&
+				Math.abs(this.bullets[i].Y - this.user1.Y - this.user1.heigth/2) < this.user1.heigth/2) {
+				this.user1.health--;
+				this.bullets[i].enable = false;
+			}
+			if (this.user2 &&
+				Math.abs(this.bullets[i].X - this.user2.X - this.user2.width/2) < this.user2.width/2 &&
+				Math.abs(this.bullets[i].Y - this.user2.Y - this.user2.heigth/2) < this.user2.heigth/2) {
+				this.user2.health--;
+				this.bullets[i].enable = false;
+			}
+			
 			if (!this.bullets[i].enable) {
-				this.bullets.shift();
+				this.bullets[i] = undefined;
 			} else {
 				this.bullets[i].draw();
 			}
@@ -292,25 +379,58 @@ engine.prototype = {
 				this.audio.fly_high1.pause();
 				this.audio.fly_high2.pause();
 				this.audio.explode.play();
+				this.animations.push(new Animation(this.ctx, this.images['explosion'], 64, 0.2, this.user1.X, this.user1.Y));
 				this.user1 = null;
-				//alert("You lose!");
+				this.user1Resp = 0;
+				this.scores.user2++;
 			} else {
+				for (var i = 0; i < this.user1.health; i++) {
+					this.ctx.drawImage(this.images['heart'], 5 + i*(this.images['heart'].width + 5), 5);
+				}
 				this.user1.draw();
 			}
+		} else {
+			this.user1Resp += dt;
+			if (this.user1Resp >= this.respTime) {
+				this.user1 = new Biplane(this.ctx, this.images['bp1'], this.images['bullet'], true, this.bullets);
+			}
 		}
+		
 		if (this.user2 != null) {
 			this.user2.step(dt);
 			if (!this.user2.isAlive) {
 				this.audio.fly_high1.pause();
 				this.audio.fly_high2.pause();
 				this.audio.explode.play();
+				this.animations.push(new Animation(this.ctx, this.images['explosion'], 64, 0.1, this.user2.X, this.user2.Y));
 				this.user2 = null;
-				//alert("You lose!");
+				this.user2Resp = 0;
+				this.scores.user1++;
 			} else {
+				for (var i = 0; i < this.user2.health; i++) {
+					this.ctx.drawImage(this.images['heart'], 635 - (i+1)*(this.images['heart'].width + 5), 5);
+				}
 				this.user2.draw();
 			}
+		} else {
+			this.user2Resp += dt;
+			if (this.user2Resp >= this.respTime) {
+				this.user2 = new Biplane(this.ctx, this.images['bp2'], this.images['bullet'], false, this.bullets);
+			}
 		}
-		
+
+		for (var i = 0; i < this.animations.length; i++) {
+			if (!this.animations[i])
+				continue;
+			this.animations[i].tick(dt);
+						
+			if (this.animations[i].finished) {
+				this.animations[i] = undefined;
+			} else {
+				this.animations[i].draw();
+			}
+		}
+			
         for(var i = 0; i < this.biplanes.length; i++) {
 			if (!this.biplanes[i].isAlive) {
 				this.audio.fly_high1.pause();
@@ -320,7 +440,7 @@ engine.prototype = {
 			this.biplanes[i].step(dt);
 			this.biplanes[i].draw();
 		}
-		
+		/*
 		if (this.user2 != null) {
 			this.ctx.fillText('Speed: '+this.user2.V+' ('+this.user2.vx+', '+this.user2.vy+')', 5, 20);
 			this.ctx.fillText('Pos: ('+this.user2.X+', '+this.user2.Y+')', 5, 40);
@@ -328,7 +448,7 @@ engine.prototype = {
 			this.ctx.fillText('Force: '+this.user2.F, 5, 80);
 			this.ctx.fillText('Sky/Fly/Alive: '+this.user2.isSky+'/'+this.user2.isFlying+'/'+this.user2.isAlive, 5, 100);
 		}
-
+		*/
         if (this.paused) {
             this.ctx.fillText("Press SpaceBar to play!", 10, this.ctx.height - 20);
         }
@@ -346,8 +466,8 @@ engine.prototype = {
     {
         var that = this;
 		this.curTime = (new Date()).getTime();
-		this.user1 = new biplane(this.ctx, "images/biplan1.png", "images/bullet.png", true, this.bullets); 
-		this.user2 = new biplane(this.ctx, "images/biplan2.png", "images/bullet.png", false, this.bullets); 
+		this.user1 = new Biplane(this.ctx, this.images['bp1'], this.images['bullet'], true, this.bullets); 
+		this.user2 = new Biplane(this.ctx, this.images['bp2'], this.images['bullet'], false, this.bullets); 
         this.paused = false;
         this.redraw();
 /*		setTimeout(
